6aed8b15d96ec29a3916547efda5853496d9928e
Felix Cramer <felixcramer18@gmail.com>
2023-02-21 15:54:34 +0100

    second commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,7 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  def details
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-get "tasks" to: "tasks#index"
-
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#details', as: :task
 end
